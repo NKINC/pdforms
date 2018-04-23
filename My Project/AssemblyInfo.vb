8abebb712965bd12b3ebea7e5d00cc4ca5e0be08
NKINC <NKINC@users.noreply.github.com>
2018-04-23 10:53:29 -0400

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PdForms.Net")>
+<Assembly: AssemblyDescription("PdForms.net - an open-source pdf editor.")>
+<Assembly: AssemblyCompany("Nicholas Kowalewicz")>
+<Assembly: AssemblyProduct("PdFormsNet")>
+<Assembly: AssemblyCopyright("Copyright © 2018 Nicholas Kowalewicz. All rights reserved.")>
+<Assembly: AssemblyTrademark("PdForms.net")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0bca9ab1-1ddc-45b3-9493-979c85090be2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("4.5.2018.0420")>
+<Assembly: AssemblyFileVersion("4.5.2018.0420")>
+<Assembly: NeutralResourcesLanguage("en-US")>
