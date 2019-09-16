4d02e4a948dffc09e193e7dca8a5e41c9c750e4a
NKINC <NKINC@users.noreply.github.com>
2019-09-16 00:50:41 -0400

    Add files via upload

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.5.2019.0914")>
-<Assembly: AssemblyFileVersion("4.5.2019.0914")>
+<Assembly: AssemblyVersion("4.5.2019.0916")>
+<Assembly: AssemblyFileVersion("4.5.2019.0916")>
 <Assembly: NeutralResourcesLanguage("en-US")>
